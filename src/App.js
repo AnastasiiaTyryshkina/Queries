b202b209e737727a9a4cb6ab8e6b3555edef8c91
AnastasiiaTyryshkina <tyrnasmail@gmail.com>
2019-12-23 12:12:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Queries from './components/Queries';
+import QueryAdmin from './components/QueryAdmin';
+import Footer from './components/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+library.add(faEdit);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/queries" component={Queries} />
+              <Route exact path="/admin" component={QueryAdmin} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
